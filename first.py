d80bb878ac3b9ab1269db931fcd4c14acb46abe4
krishdhumal <krish.dhumal1209@gmail.com>
2025-06-11 10:30:55 +0530

    sec

--- first.py
+++ first.py
@@ -328,3 +328,53 @@ s1 = Sub()
 s1.disp2()  
 s1.disp3()
 """
+"""def main(fun):
+    fun()
+    print("inside caller function")
+def disp():
+    print("inside called function")
+main(disp)
+"""
+
+"""from abc import ABC, abstractmethod
+
+class Person(ABC):
+    def walk(self):
+        print("I can walk")
+    def talk(self):
+        print("I can talk")
+    def eat(self):
+        print("I can eat")
+    def sleep(self):
+        print("I can sleep")
+    @abstractmethod
+    def performduties(self):
+        pass
+
+class Teacher(Person):
+    def performduties(self):
+        print("Perform teacher's duties")
+
+class HouseWife(Person):
+    def performduties(self):
+        print("Perform HouseWife's duties")
+
+class Soldier(Person):
+    def performduties(self):
+        print("Perform Soldier's duties")
+
+
+def perform(ref):
+    ref.walk()
+    ref.talk()
+    ref.eat()
+    ref.sleep()
+    ref.performduties()
+
+
+perform(Teacher())
+perform(Soldier())
+perform(HouseWife())
+
+#ob = Person()   #  TypeError: Can't instantiate abstract class Person with abstract method performduties
+"""
