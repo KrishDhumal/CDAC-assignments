d21896267e4c3e5c61c9f832a8d2bcf70e2c0cac
krishdhumal <krish.dhumal1209@gmail.com>
2025-06-12 12:36:29 +0530

    last one

--- second.py
+++ second.py
@@ -1,4 +1,13 @@
-import ClassWork
-print(first.add(3, 4))
+class First:
+    def mainfunction(self):
+        print("address of self is\t",id(self))
+        print("type of self is\t",type(self))
+        print("inside main function")
+f1=First()
+print("address of f1 is\t",id(f1))
+f1.mainfunction()
 
-print(__name__)
+f2=First()
+print("address of f2 is\t",id(f2))
+print("let's invoke mainfunction with f2")
+f2.mainfunction()
